3481d727400e460fcc36732b333954d78b25b9e1
Alan Russell <ajrussellaudio@gmail.com>
2016-12-02 14:27:09 +0000

    add test stubs

---
+++ specs/taxi_spec.js
@@ -0,0 +1,12 @@
+var Taxi = require('../taxi.js');
+var assert = require('assert');
+
+describe("Taxi", function() {
+
+  it("should return 5");
+
+  it("should return 2");
+
+  it("should return 12");
+
+})
